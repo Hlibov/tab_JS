3109fa8d24e2b34e523fc09741f24c1fa3c53605
Hlibov <49684027+Hlibov@users.noreply.github.com>
2020-04-03 14:54:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
-// let tab = document.querySelectorAll('.info-header-tab'),
-//   info = document.querySelector('.info-header'),
-//   tabContent = document.querySelectorAll('.info-tabcontent');
+(function () {
+
+  let tab = document.querySelectorAll('.info-header-tab'),
+    info = document.querySelector('.info-header'),
+    tabContent = document.querySelectorAll('.info-tabcontent');
 
 
-// событие загрузки DOM дерева запускает функцию выполнения программы табы
-(function (tabItemLink, tabWrap, tabItemContent) {
-  window.addEventListener('DOMContentLoaded', function () {
+  // событие загрузки DOM дерева запускает функцию выполнения программы табы
+  (function (tabItemLink = tab, tabWrap = info, tabItemContent = tabContent) {
+    window.addEventListener('DOMContentLoaded', function () {
 
-    //строгий режим
+      //строгий режим
 
-    'use strict';
+      'use strict';
 
-    // скрытие контент блоков кроме первого 
+      // скрытие контент блоков кроме первого 
 
-    function hideTabContent(a) {
-      for (let i = a; i < tabItemContent.length; i++) {
-        tabItemContent[i].classList.remove('show');
-        tabItemContent[i].classList.add('hide');
-      }
-    }
-
-    hideTabContent(1);
-
-    // отображение контента таб-блока  
-
-    function showTabContent(b) {
-      if (tabItemContent[b].classList.contains('hide')) {
-        tabItemContent[b].classList.remove('hide');
-        tabItemContent[b].classList.add('show');
-      }
-    }
-
-    // делегирование событий для табов
-
-    tabWrap.addEventListener('click', function (event) {
-      let target = event.target;
-      if (target && target.classList.contains('info-header-tab')) {
-        for (let i = 0; i < tabItemLink.length; i++) {
-          if (tabItemLink[i] == target) {
-            hideTabContent(0);
-            showTabContent(i);
-            break;
-          }
+      function hideTabContent(a) {
+        for (let i = a; i < tabItemContent.length; i++) {
+          tabItemContent[i].classList.remove('show');
+          tabItemContent[i].classList.add('hide');
         }
       }
+
+      hideTabContent(1);
+
+      // отображение контента таб-блока  
+
+      function showTabContent(b) {
+        if (tabItemContent[b].classList.contains('hide')) {
+          tabItemContent[b].classList.remove('hide');
+          tabItemContent[b].classList.add('show');
+        }
+      }
+
+      // делегирование событий для табов
+
+      tabWrap.addEventListener('click', function (event) {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+          for (let i = 0; i < tabItemLink.length; i++) {
+            if (tabItemLink[i] == target) {
+              hideTabContent(0);
+              showTabContent(i);
+              break;
+            }
+          }
+        }
+      });
     });
-  });
-}(tab, info, tabContent));
+  }());
+})();
